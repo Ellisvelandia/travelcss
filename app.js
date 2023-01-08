f1598a93731e00f95107650b586f7e609484773f
Ellisvelandia <eyis619l@gmail.com>
2023-01-07 23:57:09 -0500

    responsive

---
+++ app.js
@@ -0,0 +1,77 @@
+
+function toggleMenu() {
+  var aside = document.querySelector("aside");
+  aside.classList.toggle("active")
+}
+
+ScrollReveal().reveal("header", {
+  duration: 1000,
+  distance: "200%",
+  origin: "top",
+  opacity: null
+})
+
+
+ScrollReveal().reveal(".contenido", {
+  duration: 3000,
+  distance: "400%",
+  origin: "right",
+  opacity: null
+})
+
+
+ScrollReveal().reveal("#card1", {
+  duration: 5000,
+  distance: "400%",
+  origin: "bottom",
+  opacity: null
+})
+
+
+ScrollReveal().reveal("#card2", {
+  duration: 6000,
+  distance: "400%",
+  origin: "bottom",
+  opacity: null
+})
+
+
+ScrollReveal().reveal("#card3", {
+  duration: 7000,
+  distance: "400%",
+  origin: "bottom",
+  opacity: null
+})
+
+
+
+ScrollReveal().reveal("#box1", {
+  duration: 5000,
+  distance: "400%",
+  origin: "top",
+  opacity: null
+})
+
+
+ScrollReveal().reveal("#box2", {
+  duration: 6000,
+  distance: "400%",
+  origin: "right",
+  opacity: null
+})
+
+
+ScrollReveal().reveal("#box3", {
+  duration: 7000,
+  distance: "400%",
+  origin: "bottom",
+  opacity: null
+})
+
+
+ScrollReveal().reveal(".redesSociales", {
+  duration: 8000,
+  distance: "300%",
+  origin: "left",
+  opacity: null
+}) 
